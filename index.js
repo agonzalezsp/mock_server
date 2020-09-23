409ab3b8585e5d8b68ab611d794df5fe74dbaf3c
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2020-09-23 17:57:35 -0500

    get y post

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/recurso', function (req, res) {
+  // aqui te pinto tu solicitud.
+  console.log("Cabeceras",req.headers);
+  console.log("Params",req.params);
+
+  res.json({response: "aqui va la respuesta"})
+})
+
+app.post('/recurso', function (req, res) {
   // aqui te pinto tu solicitud.
   console.log("Cabeceras",req.headers);
   console.log("Params",req.params);
